34de42666f608cfa307e2b66816ff8dd77bcebb1
tfujii <5667980+tfujii@users.noreply.github.com>
2021-12-28 00:41:01 -0500

    comments for image schema

--- Steeltype.OpenGrapher/KnownSchemas/ImageSchema.cs
+++ Steeltype.OpenGrapher/KnownSchemas/ImageSchema.cs
@@ -1,26 +1,71 @@
 ﻿namespace Steeltype.OpenGrapher.KnownSchemas
 {
+    /// <summary>
+    /// The schema "og:image" as described on https://ogp.me/
+    /// </summary>
     public class ImageSchema : IKnownSchema
     {
+        /// <summary>
+        /// Gets or sets the image.
+        /// </summary>
+        /// <value>
+        /// An image URL which should represent your object within the graph.
+        /// </value>
         [OpenGraphProperty(@"og:image")]
-        public string Image { get; set; }
+        public string? Image { get; set; }
 
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>
+        /// Identical to og:image.
+        /// </value>
         [OpenGraphProperty(@"og:image:url")]
-        public string Url { get; set; }
+        public string? Url { get; set; }
 
+        /// <summary>
+        /// Gets or sets the secure URL.
+        /// </summary>
+        /// <value>
+        /// An alternate url to use if the webpage requires HTTPS.
+        /// </value>
         [OpenGraphProperty(@"og:image:secure_url")]
-        public string SecureUrl { get; set; }
+        public string? SecureUrl { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type of the MIME.
+        /// </summary>
+        /// <value>
+        /// A MIME type for this image.
+        /// </value>
         [OpenGraphProperty(@"og:image:type")]
-        public string MimeType { get; set; }
+        public string? MimeType { get; set; }
 
+        /// <summary>
+        /// Gets or sets the width.
+        /// </summary>
+        /// <value>
+        /// The number of pixels wide.
+        /// </value>
         [OpenGraphProperty(@"og:image:width")]
         public int? Width { get; set; }
 
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        /// <value>
+        /// The number of pixels high.
+        /// </value>
         [OpenGraphProperty(@"og:image:height")]
         public int? Height { get; set; }
 
+        /// <summary>
+        /// Gets or sets the image alt.
+        /// </summary>
+        /// <value>
+        /// A description of what is in the image (not a caption). If the page specifies an og:image it should specify og:image:alt.
+        /// </value>
         [OpenGraphProperty(@"og:image:alt")]
-        public string ImageAlt { get; set; }
+        public string? ImageAlt { get; set; }
     }
 }
